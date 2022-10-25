9ed03e66f2b65bb5c9099bcfa57636048f7ae587
Jordy Williams <jordy_williams@hotmail.co.uk>
2022-10-25 12:15:03 +1100

    feat: pass `isPlayingRef` to oscillators and `playingRefArray`

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import InteractiveOscillator from "./oscillator";
 import GlobalPlayPause from "./globalPlayPause";
+import React, { useRef } from "react";
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 export default function App() {
+  const osc1PlayingRef = useRef(false);
+  const osc2PlayingRef = useRef(false);
+  const osc3PlayingRef = useRef(false);
+  // TODO: set these nicer
+
   const osc1 = new InteractiveOscillator({
     initOscType: "sine",
     initFreq: "73",
     minFreq: "20",
     maxFreq: "1000",
     id: "osc-1",
+    isPlayingRef: osc1PlayingRef,
   });
   const osc2 = new InteractiveOscillator({
     initOscType: "square",
     initFreq: "77",
     minFreq: "20",
     maxFreq: "1000",
-    id: "osc-1",
+    id: "osc-2",
+    isPlayingRef: osc2PlayingRef,
   });
   const osc3 = new InteractiveOscillator({
     initOscType: "triangle",
     initFreq: "75",
     minFreq: "20",
     maxFreq: "1000",
-    id: "osc-1",
+    id: "osc-3",
+    isPlayingRef: osc3PlayingRef,
   });
   const globalPP = new GlobalPlayPause({
-    oscArray: [osc1, osc2, osc3],
+    playingRefArray: [osc1PlayingRef, osc2PlayingRef, osc3PlayingRef],
   });
   return (
     <div
