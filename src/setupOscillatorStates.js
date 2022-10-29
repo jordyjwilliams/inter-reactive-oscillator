103d27ec85ec2774a35917ecf6c05cd7d709d5a2
Jordy Williams <jordy_williams@hotmail.co.uk>
2022-10-30 10:57:34 +1100

    FEAT: create `SetupStates` function

---
+++ src/setupOscillatorStates.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+/**
+ * Sets up react states for oscillator.
+ * @component
+ * @example
+ * const freq = 440
+ * const oscType = "sine"
+ *
+ * const oscRef = useRef() // Typically defined with `SetupOscillator`
+ * const { freqState, oscType, gainState } = SetupStates({initFreq:freq, initOscType:oscType});
+ */
+export default function SetupStates(props) {
+  const [freq, setFreq] = useState(props.initFreq);
+  const [oscType, setOscType] = useState(props.initOscType);
+  const [gain, setGain] = useState(1);
+
+  return {
+    freqState: { get: freq, set: setFreq },
+    oscType: { get: oscType, set: setOscType },
+    gainState: { get: gain, set: setGain },
+  };
+}
+
+SetupStates.propTypes = {
+  /** Initial frequency of oscillator [Hz] */
+  initFreq: PropTypes.number,
+  /** Initial oscillator waveshape */
+  // TODO: set this from the oscillatorTypes struct
+  initOscType: PropTypes.oneOf(["sine", "square", "sawtooth", "triangle"]),
+};
