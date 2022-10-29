fc2d61b69f2b7316cb755e9a38355a5e4fc04ecb
Jordy Williams <jordy_williams@hotmail.co.uk>
2022-10-29 19:43:46 +1100

    feat: add `globalPlayPause` comp

--- src/globalPlayPause.js
+++ src/globalPlayPause.js
@@ -1,24 +1,38 @@
 import "./audioStyles.scss";
+import React, { useState, useEffect } from "react";
 
 export default function GlobalPlayPause(props) {
-  // playStates array of each InteractiveOscillator
+  // props.playStates: array of each InteractiveOscillator
   // isPlaying and setPlaying keys
-  const handleChange = () => {
-    props.playStates[0].setPlaying((play) => !play);
-    // props.playStates.forEach((oscPlayData) => {
-    //   oscPlayData.setPlaying(!oscPlayData.isPlaying);
-    // });
+  const [isPlayingGlobal, setPlayingGlobal] = useState(false);
+
+  // Get state changes of oscillators
+  useEffect(() => {
+    setPlayingGlobal(
+      props.playStates.map((oscPlayData) => oscPlayData.isPlaying).some(Boolean)
+    );
+  }, [props.playStates, isPlayingGlobal]);
+
+  const handleGlobalPlayPause = () => {
+    console.log(`All oscillators ` + (isPlayingGlobal ? "stopped" : "started"));
+    props.playStates.map((oscPlayData) =>
+      oscPlayData.setPlaying(!isPlayingGlobal)
+    );
   };
-  const isPlaying = false;
 
   return (
     <div>
-      <button
-        onClick={handleChange}
-        // data-playing={isPlaying}
-        id={`global-play-pause`}
-        className={isPlaying ? "play-pause-button paused" : "play-pause-button"}
-      />
+      <label htmlFor="global-play-pause">
+        Global Controls:
+        <br />
+        <button
+          onClick={handleGlobalPlayPause}
+          id="global-play-pause"
+          className={
+            isPlayingGlobal ? "play-pause-button paused" : "play-pause-button"
+          }
+        />
+      </label>
     </div>
   );
 }
