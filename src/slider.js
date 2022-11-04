85c83ae5417060f09ff5f69493435000e8c91c14
Jordy Williams <jordy_williams@hotmail.co.uk>
2022-11-04 12:13:02 +1100

    feat: `Slider` docstring and `PropTypes`

--- src/slider.js
+++ src/slider.js
@@ -1,5 +1,32 @@
-export default function Slider({ min, max, step, val, onSlide, label, id }) {
-  // requires min, mix, val, onSlider, label
+import PropTypes from "prop-types";
+/**
+ * Slider react component.
+ * @component
+ * @example
+ * const [getFreq, setFreq] = useState(440);
+ *
+ * const handleStateChange = (event, cb) => {
+ *   console.log(`${event.target.id} set to ${event.target.value}`);
+ *   cb(event.target.value);
+ * };
+ *
+ * const freqSlider = new Slider({
+ *   val: getFreq,
+ *   onSlide: (e) => handleStateChange(e, freqState.set),
+ *   min: 20,
+ *   max: 20000,
+ *   label: `Frequency [Hz] `,
+ *   id: `my-freq-slider`,
+ * });
+ * // Rendering:
+ * return(
+ *    <div id="freq-slider-div" htmlFor="my-freq-slider">
+ *      {freqSlider}
+ *    </div>
+ * )
+ */
+
+export default function Slider(props) {
   return (
     <div>
       <br />
@@ -27,3 +54,20 @@ export default function Slider({ min, max, step, val, onSlide, label, id }) {
     </div>
   );
 }
+
+Slider.propTypes = {
+  /** Minimum slider value */
+  min: PropTypes.number,
+  /** Maximum slider value */
+  max: PropTypes.number,
+  /** Slider step size */
+  step: PropTypes.number,
+  /** Initial slider value */
+  val: PropTypes.number,
+  /** Function called on slider change */
+  onSlide: PropTypes.func,
+  /** Slider label content */
+  label: PropTypes.string,
+  /** id */
+  id: PropTypes.string,
+};
